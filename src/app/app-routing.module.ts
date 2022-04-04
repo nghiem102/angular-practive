4b8e1782a50c4211de2baf902226862649fd879c
nghiem102 <ledinhnghiem102@gmail.com>
2022-04-04 10:44:11 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { WebsiteLayoutComponent } from './layouts/website-layout/website-layout.component';
+import { DetailComponent } from './pages/client/detail/detail.component';
+import { HomepageComponent } from './pages/client/homepage/homepage.component';
+import { ProductsComponent } from './pages/client/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WebsiteLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomepageComponent
+      },
+      {
+        path: 'product',
+        component:ProductsComponent
+      },
+      {
+        path: 'detail',
+        component: DetailComponent
+      }
+    ]
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { WebsiteLayoutComponent } from './layouts/website-layout/website-layout.component';
+import { HeaderComponent } from './components/client/header/header.component';
+import { FooterComponent } from './components/client/footer/footer.component';
+import { HomepageComponent } from './pages/client/homepage/homepage.component';
+import { ProductsComponent } from './pages/client/products/products.component';
+import { DetailComponent } from './pages/client/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent,
+    WebsiteLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    ProductsComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
