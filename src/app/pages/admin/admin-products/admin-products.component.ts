e0e0bf51ebb372816316951af650b5f99d160fae
nghiem102 <ledinhnghiem102@gmail.com>
2022-04-12 23:50:55 +0700

    products

---
+++ src/app/pages/admin/admin-products/admin-products.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-admin-products',
+  templateUrl: './admin-products.component.html',
+  styleUrls: ['./admin-products.component.css']
+})
+export class AdminProductsComponent implements OnInit {
+  products: any
+  constructor(private ps: ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts()
+  }
+
+  getProducts(): void {
+    this.ps.getProducts().subscribe(data => {
+      this.products = data
+    })
+  }
+
+  
+
+}
